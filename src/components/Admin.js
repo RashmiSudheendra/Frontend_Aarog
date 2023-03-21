6473e034e3fb443b7a26c1cfcbb0b321e82e090e
Rashmi S <110243712+RashmiSudheendra@users.noreply.github.com>
2023-03-22 00:27:19 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -63,13 +63,6 @@ function Admin(users) {
             {/* Page Heading */}
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
               <h1 className="h3 mb-0 text-gray-800">Admin Dashboard</h1>
-              <p
-                href="Javascript(void)"
-                className="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm"
-              >
-                <i className="fas fa-download fa-sm text-dark-50"></i> Generate
-                Report
-              </p>
             </div>
 
             {/* Content Row */}
