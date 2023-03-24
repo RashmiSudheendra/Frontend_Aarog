0d167ba92e128d3be419a0e042e8a8e36f715e83
Rashmi S <110243712+RashmiSudheendra@users.noreply.github.com>
2023-03-24 13:16:29 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -47,7 +47,7 @@ function Profile() {
         <div>
           <Form>
           <div className="d-flex justify-content-center w-100">
-          <div className="card m-5 d-flex justify-content-center" style={{'width':'30vw'}}>
+          <div className="card m-5 d-flex justify-content-center w-50">
             <div className="d-flex justify-content-center w-100">
               <Image src={user[0].picture} alt="Profile-Pic"
                   className="rounded-circle img-fluid my-4" style={{width: '115px'}}/>
