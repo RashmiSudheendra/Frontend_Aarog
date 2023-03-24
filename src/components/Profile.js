8f40e8640f63557078b6f1c1eeda1f5b6476b847
Rashmi S <110243712+RashmiSudheendra@users.noreply.github.com>
2023-03-24 13:12:08 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -47,7 +47,7 @@ function Profile() {
         <div>
           <Form>
           <div className="d-flex justify-content-center w-100">
-          <div className="card m-5 d-flex justify-content-center" style={{'width':'35vw'}}>
+          <div className="card m-5 d-flex justify-content-center" style={{'width':'30vw'}}>
             <div className="d-flex justify-content-center w-100">
               <Image src={user[0].picture} alt="Profile-Pic"
                   className="rounded-circle img-fluid my-4" style={{width: '115px'}}/>
@@ -61,8 +61,8 @@ function Profile() {
                 <Form.Control id='userName' plaintext type="text" placeholder="Enter Name" defaultValue={user[0].name}/>
                 </div>
               </div>
-              <hr className='border border-info border-1 mb-0'/>
-              <div className="row mb-0">
+              <hr className='border border-info border-1 mt-0'/>
+              <div className="row">
                 <div className="">
                   <p className="mb-0">Email</p>
                 </div>
@@ -70,8 +70,8 @@ function Profile() {
                 <Form.Control id='userEmail' plaintext type="text" placeholder="Enter Email" defaultValue={user[0].email} readOnly/>
                 </div>
               </div>
-              <hr className='border border-info border-1 mb-0'/>
-              <div className="row mb-0">
+              <hr className='border border-info border-1 mt-0'/>
+              <div className="row">
                 <div className="">
                   <p className="mb-0">Phone</p>
                 </div>
@@ -79,8 +79,8 @@ function Profile() {
                 <Form.Control id='userPhone' plaintext type="number" placeholder="Enter Mobile" defaultValue={user[0].phone}/>
                 </div>
               </div>
-              <hr className='border border-info border-1 mb-0'/>
-              <div className="row mb-0">
+              <hr className='border border-info border-1 mt-0'/>
+              <div className="row">
                 <div className="">
                   <p className="mb-0">Age</p>
                 </div>
@@ -88,8 +88,8 @@ function Profile() {
                 <Form.Control id='userAge' plaintext type="number" placeholder="Age" defaultValue={user[0].age}/>
                 </div>
               </div>
-              <hr className='border border-info border-1 mb-0'/>
-              <div className="row mb-0">
+              <hr className='border border-info border-1 mt-0'/>
+              <div className="row">
                 <div className="">
                   <p className="mb-0">Address</p>
                 </div>
@@ -97,10 +97,10 @@ function Profile() {
                 <Form.Control id='userAddress' plaintext type="text" placeholder="Address Line" defaultValue={user[0].address}/>
                 </div>
               </div>
-              <hr className='border border-info border-1 mb-0'/>
+              <hr className='border border-info border-1 mt-0'/>
             </div>
             <div className="d-flex justify-content-center w-100">
-              <button className="btn btn-info text-uppercase fw-bold mb-3 px-0 " sytle={{'width':'15vw'}} type="submit" onClick={()=>updateUser()}>
+              <button className="btn btn-info text-uppercase fw-bold mb-3 px-1 " sytle={{'width':'15vw'}} type="submit" onClick={()=>updateUser()}>
                 Update
               </button>
             </div>
