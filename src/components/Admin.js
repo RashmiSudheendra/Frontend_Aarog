4d37df3a9d812800ac72c56dfab9f948541c4163
Rashmi S <110243712+RashmiSudheendra@users.noreply.github.com>
2023-03-24 09:35:57 +0530

    Update Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,42 +1,27 @@
-import React, {useState, useEffect} from "react";
-// import BasicCard,{ProgressCard} from './Card'
+import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
-// import { ToastContainer, toast } from 'react-toastify';
 
 function Admin() {
 
   let navigate = useNavigate();
 
-  const userD = JSON.parse(localStorage.getItem('userinfo'))
+  let url = "https://newserver-wy4a.onrender.com"
 
-  console.log(userD)
+  // console.log(userD)
 
-  let [user, setUser] = useState([userD])
+  let [user, setUser] = useState([])
 
-  let url = "https://newserver-wy4a.onrender.com" 
-
-  fetch("https://newserver-wy4a.onrender.com")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      setUser(data);
-      localStorage.setItem("userinfo", JSON.stringify(data))
-    })
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://newserver-wy4a.onrender.com")
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data);
-      setUser(data);
-      // localStorage.setItem("userinfo", JSON.stringify(data))
-    });
-  },[user])
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, [user])
 
   console.table(user)
-  // const userD = Object.values(users || {})
   const len = user.length;
   async function deleteNewUserData(i) {
     console.log(i)
@@ -50,52 +35,45 @@ function Admin() {
     console.log(res);
     navigate('/admin')
   }
-  
 
-  function logoutAsAdmin(){
+
+  function logoutAsAdmin() {
     navigate('/signin')
   }
-  // console.log(props.data)
   return (
     <>
-      {/* Content Wrapper*/}
-      <div id="content-wrapper" className="d-flex flex-column">
-        {/* Main Content */}
+      <div id="content-wrapper" className="d-flex flex-column justify-content-between" style={{ 'height': '100vh' }}>
         <div id="content">
-          {/* Begin Page Content */}
           <div className="container-fluid">
-            {/* Page Heading */}
             <div className="d-sm-flex align-items-center justify-content-center mb-0">
               <h1 className="h3 mt-5 text-gray-800">Admin Dashboard</h1>
             </div>
-
-            {/* Content Row */}
             <div className="row">
               <div className="d-flex justify-content-between mb-4 mt-2">
                 <div className="bg-info border border-5 border-info rounded-2 shadow p-2">
-                    <div className="row no-gutters align-items-center w-100 mx-2">
-                      <div className="col mr-2 px-0">
-                        <div className="font-weight-bold text-dark mb-1">
-                          <h5>No of users</h5>
-                        </div>
-                        <div className="row no-gutters align-items-center">
-                          <div className="col-auto">
-                            <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                              {len}
-                            </div>
+                  <div className="row no-gutters align-items-center w-100 mx-2">
+                    <div className="col mr-2 px-0">
+                      <div className="font-weight-bold text-dark mb-1">
+                        <h5>No of users</h5>
+                      </div>
+                      <div className="row no-gutters align-items-center">
+                        <div className="col-auto">
+                          <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                            {len}
                           </div>
                         </div>
                       </div>
-                      <div className="col-auto">
-                        <i className="fas fa-user fa-2x text-gray-300"></i>
-                      </div>
                     </div>
+                    <div className="col-auto">
+                      <i className="fas fa-user fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
                 </div>
                 <div className="d-flex justify-content-end" id="signindiv">
-                <button className="btn btn-info text-uppercase fw-bold mb-3 h-50 " type="submit" onClick={() => logoutAsAdmin()}>
-                  Sign Out
-                </button>
-              </div>
+                  <button className="btn btn-info text-uppercase fw-bold mb-3 h-50 " type="submit" onClick={() => logoutAsAdmin()}>
+                    Sign Out
+                  </button>
+                </div>
               </div>
             </div>
             <div>
@@ -113,48 +91,36 @@ function Admin() {
                 </thead>
                 <tbody>
                   {
-                    user.map((e,i)=>{
-                      // componentDidMount()
-                        return <tr key={i}>
-                            <td>{i+1}</td>
-                            <td>{e.name}</td>
-                            <td>{e.email}</td>
-                            <td>{e.phone}</td>
-                            <td>{e.age}</td>
-                            <td>{e.address}</td>
-                            <td>
-                                <Button variant='danger' onClick={()=>deleteNewUserData(i)}>
-                                    <i className="fas fa-fw fa-user-slash"></i>&nbsp;&nbsp;Delete
-                                </Button>
-                            </td>
-                        </tr>
+                    user.map((e, i) => {
+                      return <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{e.name}</td>
+                        <td>{e.email}</td>
+                        <td>{e.phone}</td>
+                        <td>{e.age}</td>
+                        <td>{e.address}</td>
+                        <td>
+                          <Button variant='danger' onClick={() => deleteNewUserData(i)}>
+                            <i className="fas fa-fw fa-user-slash"></i>&nbsp;&nbsp;Delete
+                          </Button>
+                        </td>
+                      </tr>
                     })
                   }
                 </tbody>
               </Table>
             </div>
           </div>
-          {/* /.container-fluid */}
         </div>
 
-        {/* End of Main Content */}
-
-        {/* Footer */}
-        <footer className="sticky-footer bg-white">
+        <footer className="sticky-footer bg-secondary text-white mb-3">
           <div className="container my-auto">
-            <div className="copyright text-center my-auto">
-              <span>Copyright &copy; Your Website 2021</span>
+            <div className="copyright text-center my-1 fw-bold">
+              <span>Copyright &copy; Your Website 2023</span>
             </div>
           </div>
         </footer>
-        {/* End of Footer */}
-        {/* <ToastContainer 
-      position="top-center"
-      autoClose={1500}
- /> */}
       </div>
-
-      {/* End of Content Wrapper */}
     </>
   );
 }
